2cf5516a4e613bac7d24dbb2201d32d3685264ba
Rodrigo Corilla <n00394245@upn.pe>
2026-09-05 17:43:39 -0500

    chore: initialize Spring Boot project

---
+++ src/main/java/com/linde/linde_backend/LindeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linde.linde_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LindeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LindeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linde/linde_backend/LindeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linde.linde_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LindeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
